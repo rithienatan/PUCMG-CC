b2311516e1e01fc1d912428e2d608f6784820f4a
rithienatan <rithienatan@gmail.com>
2020-03-11 01:28:48 -0300

    Folder place in wrong place

--- 6º Semestre/Engenharia de Software II/eng 2/Gerente.js
+++
@@ -1,8 +0,0 @@
-class Gerente extends Pessoa {
-
-    constructor(nome, cpf, sexo, dataNascimento,email,salario,senha) {
-      super(nome, cpf, sexo, dataNascimento);
-      this.salario = salario;
-      this.senha = senha;
-    }
-}
